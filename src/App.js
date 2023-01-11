aed24c42031231c7908247ee0fa13fa73b3f6d3b
individual-ism <alexlchang@icloud.com>
2023-01-11 11:00:30 -0500

    Functional Tabs without Errors

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import { useState, useEffect } from 'react'
 import {Routes, Route} from 'react-router-dom';
 import Home from "./Home.jsx";
 import Person from './Person.jsx'
+import Nav from './Nav';
 
 function App() {
     const [people, setPeople] = useState([])
+    const [index, setIndex] = useState(0)
 
     useEffect(() => {
         const personCatcher = (id) => {
--- src/Person.jsx
+++ src/Person.jsx
@@ -1,11 +1,12 @@
 import {useState} from "react"
+import Nav from "./Nav.jsx"
 
 function Person({people}) {
     const [index, setIndex] = useState(0)
 
     if (!people.length) return <h1>Loading...</h1>
 
-    function indexAdjustment() {
+    {/*function indexAdjustment() {
         if (index < people.length) {
             setIndex(prev => prev + 1)
         } else if (index >= people.length) {
@@ -19,22 +20,41 @@ function Person({people}) {
         } else {
             return true
         }
+    }*/}
+
+    let character = people[index]
+
+    function showCharacter(index) {
+        setIndex(index)
     }
 
     return (
         <div>
-            <h1>{people[index].name}</h1>
-            <ul display="none">
-                <li>{people[index].height}</li>
-                <li>{people[index].mass}</li>
-                <li>{people[index].hair_color}</li>
-                <li>{people[index].skin_color}</li>
-                <li>{people[index].eye_color}</li>
-                <li>{people[index].gender}</li>
-                <li>{people[index].homeworld}</li>
-            </ul>
-            <button onClick={() => indexAdjustment()} disabled={disableButton()}>Next Person</button>
-            <button onClick={() => setIndex(0)}>Reset</button> 
+            <nav className="buttonCollection">
+                <button className="characterButton" onClick={() => showCharacter(0)}>Luke Skywalker</button>
+                <button className="characterButton" onClick={() => showCharacter(1)}>C-3PO</button>
+                <button className="characterButton" onClick={() => showCharacter(2)}>R2-D2</button>
+                <button className="characterButton" onClick={() => showCharacter(3)}>Darth Vader</button>
+                <button className="characterButton" onClick={() => showCharacter(4)}>Leia Organa</button>
+                <button className="characterButton" onClick={() => showCharacter(5)}>Owen Lars</button>
+                <button className="characterButton" onClick={() => showCharacter(6)}>Beru Whitesun Lars</button>
+                <button className="characterButton" onClick={() => showCharacter(7)}>R5-D4</button>
+                <button className="characterButton" onClick={() => showCharacter(8)}>Biggs Darklighter</button>
+                <button className="characterButton" onClick={() => showCharacter(9)}>Obi-Wan Kenobi</button>
+            </nav>
+            <div className="character" id={index}>
+                <h1 className="character">{character.name}</h1>
+                <ul className="character">
+                    <li className="character">Height: {character.height}</li>
+                    <li className="character">Mass: {character.mass}</li>
+                    <li className="character">Hair Color: {character.hair_color}</li>
+                    <li className="character">Skin Color: {character.skin_color}</li>
+                    <li className="character">Eye Color: {character.eye_color}</li>
+                    <li className="character">Gender: {character.gender}</li>
+                </ul>
+            </div>
+            {/* <button onClick={() => indexAdjustment()} disabled={disableButton()}>Next Person</button>
+            <button onClick={() => setIndex(0)}>Reset</button>  */}
         </div>
     )
 
